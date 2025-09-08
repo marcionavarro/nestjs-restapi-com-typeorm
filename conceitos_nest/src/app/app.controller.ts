2724512849bb83123d183d3b3fede54f21db56df
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-09-07 23:24:10 -0300

    [conceitos_nest] Seção 3: Conceitos básicos sobre Modules, Controllers e Providers (services) do NestJS - Finalizado

--- conceitos_nest/src/app/app.controller.ts
+++ conceitos_nest/src/app/app.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('home')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('hello')
   getHello(): string {
-    return this.appService.getHello();
+    return 'Qualquer coisa'; //this.appService.getHello();
+  }
+
+  @Get('exemplo')
+  exemplo() {
+    return this.appService.solucionaExemplo();
   }
 }
